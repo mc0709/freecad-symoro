9030a390b30406629a886fb42d84fc70878277c8
Gaël Ecorchard <galou_breizh@yahoo.fr>
2013-04-15 11:26:51 +0200

    Basic lexer implemented

--- read_par.py
+++ read_par.py
@@ -26,6 +26,18 @@ __author__ = "Gael Ecorchard <galou_breizh@yahoo.fr>"
 __url__ = ["http://free-cad.sourceforge.net"]
 
 import ply.lex as lex
+import ply.yacc as yacc
+
+
+def input_new(name):
+    while True:
+        try:
+            v = float(raw_input('Value for "' + name + '": '))
+        except ValueError:
+            pass
+        else:
+            break
+    return v
 
 
 def add_type(cls, t):
@@ -39,13 +51,12 @@ def add_type(cls, t):
     setattr(cls, t_type.__name__, t_type)
 
 
-class ParLex(object):
-    def __init__(self):
+class ParLexer(object):
+    def __init__(self, **kwargs):
         for k in self.keywords:
             add_type(self, k)
-
-    def build(self, **kwargs):
         self.lexer = lex.lex(module=self, **kwargs)
+        self.lookup = {}
 
     keywords = [
             'NF', 'NL', 'NJ', 'Type',
@@ -89,6 +100,18 @@ class ParLex(object):
         t.value = int(t.value)
         return t
 
+    # TODO: recognize NAME as NAME and not KEYWORD inside {}
+    def t_NAME(self, t):
+        r'[a-zA-Z_][a-zA-Z0-9_]*'
+        try:
+            t.value = self.lookup[t.value]
+        except KeyError:
+            val = input_new(t.value)
+            self.lookup[t.value] = val
+            t.value = val
+        t.type = 'FLOAT'
+        return t
+
     def t_newline(self, t):
         r'\n+'
         t.lexer.lineno += t.value.count("\n")
@@ -97,5 +120,72 @@ class ParLex(object):
         print("Illegal character '%s'" % t.value[0])
         t.lexer.skip(1)
 
-    t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
+class ParParser:
+    def __init__(self):
+        self.lexer = ParLexer()
+        self.tokens = self.lexer.tokens
+        self.lookup = self.lexer.lookup
+        #self.parser = yacc.yacc(module=self,write_tables=0,debug=False)
+        self.parser = yacc.yacc(module=self)
+        self.robot_definition = {}
+
+    def parse(self, data):
+        if data:
+            return self.parser.parse(data, self.lexer.lexer, 0, 0, None)
+        else:
+            return []
+
+    precedence = (
+        ('left','+','-'),
+        ('left','*','/'),
+        ('right','UMINUS'),
+        )
+
+    def p_error(self, p):
+        print 'Error!'
+        print p
+        print
+
+    def p_expression_binop(self, p):
+        """expression : expression '+' expression
+                    | expression '-' expression
+                    | expression '*' expression
+                    | expression '/' expression"""
+        if p[2] == '+'  :
+            p[0] = p[1] + p[3]
+        elif p[2] == '-':
+            p[0] = p[1] - p[3]
+        elif p[2] == '*':
+            p[0] = p[1] * p[3]
+        elif p[2] == '/':
+            p[0] = p[1] / p[3]
+
+    def p_expression_uminus(self, p):
+        """expression : '-' expression %prec UMINUS"""
+        p[0] = -p[2]
+
+    def p_assignment(self, p):
+        """assignment : keyword '=' '{' paramlist '}'"""
+        self.robot_definition[p[0]] = p[3]
+
+    def p_keyword(self, p):
+        """keyword :"""
+        pass
+
+    def p_paramlist(self, p):
+        """paramlist : param ',' param
+                       | param"""
+        pass
+
+if __name__ == '__main__':
+    with open('RX90_geom.par', 'r') as f:
+        rx90_geom = f.read()
+
+    parlexer = ParLexer()
+    parlexer.lexer.input(rx90_geom)
+    for tok in parlexer.lexer:
+        print(tok)
+
+    #parser = ParParser()
+    #parser.parse(rx90_geom)
