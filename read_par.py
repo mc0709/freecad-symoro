642b702c51266e62666101250a99f3e5bfa06bc9
Gaël Ecorchard <galou_breizh@yahoo.fr>
2013-04-21 02:15:05 +0200

    Implement lexer and parser
    The lexer is not perfect because I still didn't find out how to use
    states triggered by braces and, at the same time, output these braces
    as litterals.

--- read_par.py
+++ read_par.py
@@ -32,7 +32,8 @@ import ply.yacc as yacc
 def input_new(name):
     while True:
         try:
-            v = float(raw_input('Value for "' + name + '": '))
+            #v = float(raw_input('Value for "' + name + '": '))
+            v = 0
         except ValueError:
             pass
         else:
@@ -58,24 +59,24 @@ class ParLexer(object):
             ]
 
     tokens = [
-            'COMMENT', 'NAME', 'KEYWORD', 'INTEGER', 'FLOAT', 'Pi',
+            'COMMENT', 'KEYWORD', 'NUMBER', 'Pi', 'NEWLINE', 'NAME',
             ]
 
     literals = ['=','+','-','*','/', '(', ')', '{', '}', ',']
 
-    states = (
-        ('paramlist', 'inclusive'),
-    )
+    #states = (
+    #    ('paramlist', 'inclusive'),
+    #)
 
     t_ignore = " \t\r"
 
-    def t_begin_paramlist(self, t):
-        r'\{'
-        t.lexer.push_state('paramlist')
+    #def t_begin_paramlist(self, t):
+    #    r'\{'
+    #    t.lexer.push_state('paramlist')
 
-    def t_end_paramlist(self, t):
-        r'\}'
-        t.lexer.pop_state()
+    #def t_end_paramlist(self, t):
+    #    r'\}'
+    #    t.lexer.pop_state()
 
     def t_COMMENT(self, t):
         r'\(\*.*'
@@ -87,25 +88,22 @@ class ParLexer(object):
     def t_KEYWORD(self, t):
         return t
 
-    @TOKEN(_keyword_pattern)
-    def t_paramlist_KEYWORD(self, t):
-        return self.t_NAME(t)
+    # Within braces keywords are treated as names, though this is not supported
+    # by Symoro+.
+    #@TOKEN(_keyword_pattern)
+    #def t_paramlist_KEYWORD(self, t):
+    #    return self.t_NAME(t)
 
     def t_Pi(self, t):
         r'\bPi\b'
         from math import pi
         t.value = pi
-        t.type = 'FLOAT'
+        t.type = 'NUMBER'
         return t
 
-    def t_FLOAT(self, t):
+    def t_NUMBER(self, t):
         r'[0-9.eE]+'
         t.value = float(t.value)
-        return t
-
-    def t_INTEGER(self, t):
-        r'\d+'
-        t.value = int(t.value)
         return t
 
     def t_NAME(self, t):
@@ -116,49 +114,91 @@ class ParLexer(object):
             val = input_new(t.value)
             self.lookup[t.value] = val
             t.value = val
-        t.type = 'FLOAT'
+        t.type = 'NUMBER'
         return t
 
-    def t_newline(self, t):
-        r'\n+'
+    def t_NEWLINE(self, t):
+        r'[\n]+'
         t.lexer.lineno += t.value.count("\n")
+        return t
+
+    #def t_paramlist_NEWLINE(self, t):
+    #    r'[\n]+'
+    #    t.lexer.lineno += t.value.count("\n")
 
     def t_error(self, t):
         print("Illegal character '%s'" % t.value[0])
         t.lexer.skip(1)
 
 
-class ParParser:
+class ParParser(object):
     def __init__(self):
         self.lexer = ParLexer()
         self.tokens = self.lexer.tokens
         self.lookup = self.lexer.lookup
         #self.parser = yacc.yacc(module=self,write_tables=0,debug=False)
-        self.parser = yacc.yacc(module=self)
+        self.parser = yacc.yacc(module=self, debug=1)
         self.robot_definition = {}
 
     def parse(self, data):
         if data:
-            return self.parser.parse(data, self.lexer.lexer, 0, 0, None)
+            #return self.parser.parse(data, self.lexer.lexer, 0, 0, None)
+            return self.parser.parse(data, self.lexer.lexer,
+                    False, 0, None)
         else:
             return []
 
     precedence = (
+        ('left', ','),
         ('left','+','-'),
         ('left','*','/'),
         ('right','UMINUS'),
         )
 
-    def p_error(self, p):
-        print 'Error!'
-        print p
-        print
+    def p_input(self, p):
+        """input :
+                | input line"""
+        pass
+
+    def p_line(self, p):
+        """line : assignment NEWLINE
+                | NEWLINE"""
+        pass
+
+    def p_assignment(self, p):
+        """assignment : KEYWORD '=' param
+                      | KEYWORD '=' '{' NEWLINE paramlist '}'"""
+        print('in p_assignment, p = {}'.format(list(p)))
+        if len(p) == 4:
+            # assignment : KEYWORD '=' param
+            self.robot_definition[p[1]] = p[3]
+        else:
+            # assignment : KEYWORD '=' '{' NEWLINE paramlist '}'
+            self.robot_definition[p[1]] = p[5]
+
+    def p_paramlist(self, p):
+        """paramlist : paramlist ',' param
+                       | param"""
+        print('in p_paramlist, p = {}'.format(list(p)))
+        if len(p) == 4:
+            # paramlist : paramlist ',' param
+            p[0] = p[1] + [p[3]]
+        else:
+            p[0] = [p[1]]
+
+    def p_param(self, p):
+        """param : expression
+                 | '(' expression ')'"""
+        print('in p_param, p = {}'.format(list(p)))
+        index = 1 if len(p) == 2 else 2
+        p[0] = p[index]
 
     def p_expression_binop(self, p):
         """expression : expression '+' expression
                     | expression '-' expression
                     | expression '*' expression
                     | expression '/' expression"""
+        print('in p_expression_binop, p = {}'.format(list(p)))
         if p[2] == '+'  :
             p[0] = p[1] + p[3]
         elif p[2] == '-':
@@ -167,34 +207,37 @@ class ParParser:
             p[0] = p[1] * p[3]
         elif p[2] == '/':
             p[0] = p[1] / p[3]
+        elif p[1] == '(' and p[3] == ')':
+            p[0] = p[2]
 
     def p_expression_uminus(self, p):
         """expression : '-' expression %prec UMINUS"""
+        print('in p_expression_uminus, p = {}'.format(list(p)))
         p[0] = -p[2]
 
-    def p_assignment(self, p):
-        """assignment : keyword '=' '{' paramlist '}'"""
-        self.robot_definition[p[0]] = p[3]
+    def p_expression_term(self, p):
+        """expression : NAME
+                    | NUMBER
+                    | Pi"""
+        print('in p_expression_term, p = {}'.format(list(p)))
+        p[0] = p[1]
 
-    def p_keyword(self, p):
-        """keyword : """
-        pass
-
-    def p_paramlist(self, p):
-        """paramlist : param ',' param
-                       | param"""
-        pass
+    def p_error(self, p):
+        print 'Error!'
+        print p
+        print
 
 if __name__ == '__main__':
     with open('RX90_geom.par', 'r') as f:
         rx90_geom = f.read()
 
-    parlexer = ParLexer()
-    parlexer.lexer.input(rx90_geom)
-    for tok in parlexer.lexer:
-        print(tok)
+    #parlexer = ParLexer()
+    #parlexer.lexer.input(rx90_geom)
+    #for tok in parlexer.lexer:
+    #    print(tok)
 
-    #parser = ParParser()
-    #parser.parse(rx90_geom)
+    parser = ParParser()
+    parser.parse(rx90_geom)
+    print parser.robot_definition
 
 
